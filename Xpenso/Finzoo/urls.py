2f489bec78520fcd54c384806510dfd521fb52d2
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-28 03:59:33 +0530

    28-06-25

--- Xpenso/Finzoo/urls.py
+++ Xpenso/Finzoo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.expense_list, name='index'),
     path('add/', views.add_expense, name='add_expense'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
 ]
