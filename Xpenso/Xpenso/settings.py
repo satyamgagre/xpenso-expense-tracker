e8638585729dfde4fe3ee6d271142a28d35729ac
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-30 02:20:18 +0530

    30-06-2025

--- Xpenso/Xpenso/settings.py
+++ Xpenso/Xpenso/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Finzoo',
     'widget_tweaks',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
